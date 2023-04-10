5defd9001b1452e8b8df7cfb6640ac4d8e6013d7
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-10 21:45:15 +0200

    guards

--- src/services/route-guard.service.ts
+++ src/services/route-guard.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthappService } from './authapp.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouteGuardService {
+export class RouteGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private BasicAuth: AuthappService) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    throw new Error('Method not implemented.');
+
+    //verifica se l'utente è loggato e restituisce true altrimenti restituirà false
+    //quindi questa è la condizione per usare guard e quindi accedere alle rotte
+    return this.BasicAuth.isLogged();
+  }
 }
