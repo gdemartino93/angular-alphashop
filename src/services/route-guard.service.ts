db98d0f7ef609f30b0bfd5b862bc02ae3e88031e
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-10 22:02:13 +0200

    redirect to login on route protected

--- src/services/route-guard.service.ts
+++ src/services/route-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthappService } from './authapp.service';
 
@@ -8,11 +8,19 @@ import { AuthappService } from './authapp.service';
 })
 export class RouteGuardService implements CanActivate {
 
-  constructor(private BasicAuth: AuthappService) { }
+  constructor(private BasicAuth: AuthappService, private route:Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    //usiamo questo per evitare che se la rotta è protette si viualizzi una pagina bianca.
+    //cosi' facendo se l'utente tenta di accedere ad una rotta protetta verrà carica la rotta login e non la pagina bianca come nell'esempio sotto
+    if(!this.BasicAuth.isLogged()){
+      this.route.navigate(['login']);
+      return false;
+    }else{
+      return true;
+    }
     //verifica se l'utente è loggato e restituisce true altrimenti restituirà false
     //quindi questa è la condizione per usare guard e quindi accedere alle rotte
-    return this.BasicAuth.isLogged();
+    // return this.BasicAuth.isLogged();
   }
 }
