a6874bf0f03c8c40a00a6561921de8342465b987
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-10 16:44:18 +0200

    create header with bootstrap

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  dataCorrente : Date = new Date
   constructor() { }
 
   ngOnInit(): void {
