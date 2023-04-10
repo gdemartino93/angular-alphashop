c8d29395bef8b176bef5ec3d581a962e40c667f1
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-10 15:13:03 +0200

    handling error if route dont exist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    component: ErrorComponent
+  },
   {
     path: 'login',
     component: LoginComponent
