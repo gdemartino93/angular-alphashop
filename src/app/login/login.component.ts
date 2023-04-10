0f1cd4446019d2488aaec68d0f20718475269f24
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-10 14:36:31 +0200

    using ngmodel

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  userId = "Luca"
+  password = ""
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  gestAut(){
+    alert(this.userId)
+  }
 }
